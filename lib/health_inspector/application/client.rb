1b03e954a788f81060bd6554f86f3c4c2fa410db
John Kerry <jk185160@ncr.com>
2016-08-09 01:10:07 -0400

    Added config entry for remedy directory and tests

--- lib/health_inspector/application/client.rb
+++ lib/health_inspector/application/client.rb
@@ -1,12 +1,25 @@
+require 'mixlib/cli'
 class HealthInspector
   # top level documentation
   class Client
+    include Mixlib::CLI
+
+    option(
+      :remedy_directory,
+      short: '-r PATH',
+      long: '--remedy_directory PATH',
+      description: 'Set the location to search for remedies',
+      required: false,
+      default: './remedies'
+    )
+
     def run
+      parse_options
       load_remedies
     end
 
     def load_remedies
-      true
+      puts config[:remedy_directory]
     end
   end
 end
--- spec/unit/application/client_spec.rb
+++ spec/unit/application/client_spec.rb
@@ -6,7 +6,21 @@ describe HealthInspector::Client do
     client
   end
 
+  before do
+    @original_argv = ARGV.dup
+    ARGV.clear
+  end
+
+  after do
+    ARGV.replace(@original_argv)
+  end
+
   describe 'run' do
+    it 'should call parse_options' do
+      argv = []
+      expect(health_inspector).to receive(:parse_options).and_return(argv)
+      health_inspector.run
+    end
     it 'should call load_remedies' do
       expect(health_inspector).to receive(:load_remedies).and_return(true)
       health_inspector.run
